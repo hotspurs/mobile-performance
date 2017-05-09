ef8250056cf62433787a0f38ffa677e49d678bb1
Vlad Dubov <dubov.web@gmail.com>
2017-05-09 13:25:01 +0300

    swift: base app

---
+++ swift/mobile performance ios swift/mobile performance ios swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mobile performance ios swift
+//
+//  Created by Владислав Дубов on 09/05/2017.
+//  Copyright © 2017 Владислав Дубов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
